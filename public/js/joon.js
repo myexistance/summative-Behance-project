eda5a24fc6f1159b592fffde6be8c3764005a292
Joon Yoon <37794149+myexistance@users.noreply.github.com>
2019-04-05 00:35:32 +1300

    jquery image tags fixed

--- public/js/joon.js
+++ public/js/joon.js
@@ -18,7 +18,6 @@ var $container = $("#BehanceProject");
 
   function insertProject(projectData) {
     // $covers.append('<img src=' + projectData.covers['404'] + ' />');
-    $container.append('<div class="project__item">'+'<img src="https:\/\/mir-s3-cdn-cf.behance.net\/projects\/404\/1769a123786915.Y3JvcCwyNDAxLDE4NzcsMjIzLDA.jpg" alt="">'+'</div>');
     $container.append('<div class="project__item">'+projectData.name+'</div>');
     $container.append('<div class="project__item">'+projectData.fields+'</div>');
     $container.append('<div class="project__item">' + '<img src= "' + projectData.covers[404] + '">')
@@ -27,7 +26,7 @@ var $container = $("#BehanceProject");
 
   $.ajax({
     dataType: "jsonp",
-    url: "https://www.behance.net/v2/users/matiascorea/projects?api_key=",
+    url: "https://www.behance.net/v2/users/matiascorea/projects?api_key=dSoSYYcQQTsMHJ9O5hWkN6gns4aV5gOM",
     success: success
   });
 });
