6ac1284f720df7d51d2b05a6e8e42d97ac307599
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2019-04-05 04:10:27 +0000

    api with 3 users showing

--- public/js/joon.js
+++ public/js/joon.js
@@ -12,15 +12,15 @@ var container = $("#BehanceProject");
   function success(data) {
 
     console.log(data)
-    // for (var i = 0; i < 3; i++) {
-    //   console.log(data.users[i])
-    //   for (var j = 0; j < 3; j++) {
-    //     var project = data.users[i].images;
-    //      console.log(project)
-    //      insertProject(project);
-    //   }
-    //
-    // }
+     for (var i = 0; i < 3; i++) {
+    console.log(data.projects[i]);
+       for (var j = 0; j < 3; j++) {
+         var project = data.projects[i];
+          console.log(project)
+          insertProject(project);
+       }
+    
+     }
 
   }
 
@@ -28,25 +28,27 @@ var container = $("#BehanceProject");
     // container.append('<img src=' + projectData.covers['404'] + ' />');
     container.append('<div class="project__item">'+projectData.name+'</div>');
     container.append('<div class="project__item">'+projectData.fields+'</div>');
-    container.append('<div class="project__item">' + '<img src= "' + projectData.users.images[230] + '">')
+    container.append('<div class="project__item">' + '<img src= "' + projectData.covers[404] + '">')
 
   }
-var user1= "peter";
-var user2="luca";
-var user3="alex";
-for (var i=1; i<4;i++){
+var user= ["matiascorea","lucaviola","raewynbrandon"];
+console.log(user[0],user[1],user[2])
+var i;
+for (i=0; i<3; i++){
   console.log(i);
-  user=user[i]
-  console.log(user);
-  var url='https://api.behance.net/v2/users?q="+user+"&client_id=dSoSYYcQQTsMHJ9O5hWkN6gns4aV5gOM';
+  console.log(user)
+  console.log(user[i]);
+ var newuser=user[i];
+  console.log(newuser);
+  var url='https://api.behance.net/v2/users/'+newuser+'/projects?client_id=dSoSYYcQQTsMHJ9O5hWkN6gns4aV5gOM';
   console.log(url);
   $.ajax({
     url: url,
     dataType: "jsonp",
     type: "GET",
-    success: function(myData){
-
-      console.log(myData);
+    success: success,
+    error:function(){
+      console.log('error');
     }
   });
 }
